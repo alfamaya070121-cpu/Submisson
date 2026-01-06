9ff6cf18bb4a420106c29e01ad08335bb6ba7243
alfamaya070121-cpu <alfamaya070121@gmail.com>
2026-01-06 11:09:23 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1WXhVRzYkv2EM5USoMgTM5n2IOdkaG8Cp
 """
 
-pip install streamlit pandas matplotlib
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
